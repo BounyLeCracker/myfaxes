fc8f860c70b8ac72349649dcb6f2193b4575cc1c
bouny <ousmanoubounyamine@gmail.com>
2024-03-22 02:39:21 +0100

    commit

--- myfaxes/static/js/script.js
+++ myfaxes/static/js/script.js
@@ -1,12 +1,31 @@
-// Sélectionnez l'icône du menu hamburger et le menu
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+// JavaScript
+// Sélectionner le menu hamburger, le menu de navigation et les liens du menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
 
-// Fonction pour basculer la classe 'active' sur le menu
-function toggleMenu() {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-}
+// Ajouter un écouteur d'événement au clic sur chaque lien du menu
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    // Désactiver l'élément input
+    document.getElementById("toggle").checked = false;
+    // Fermer le menu responsive
+    showResponsiveMenu();
+  });
+});
 
-// Écouteur d'événement pour le clic sur l'icône du menu hamburger
-hamburger.addEventListener('click', toggleMenu);
+// Définir la fonction qui affiche ou masque le menu responsive
+function showResponsiveMenu() {
+  var menu = document.getElementById("topnav_responsive_menu");
+  var icon = document.getElementById("topnav_hamburger_icon");
+  var root = document.getElementById("root");
+  if (menu.className === "") {
+    menu.className = "open";
+    icon.className = "open";
+    root.style.overflowY = "hidden";
+  } else {
+    menu.className = "";
+    icon.className = "";
+    root.style.overflowY = "";
+  }
+} // Ajouter une parenthèse fermante ici
